1262c89dfc74e51d1ca049b6e75025f0212eb61e
Fredrick10651 <Fredrick.mutsotso@student.moringaschool.com>
2021-08-24 19:14:54 +0300

    Add hashcode and overide methods in our java class

--- glam-your-space/src/main/java/models/Feedback.java
+++ glam-your-space/src/main/java/models/Feedback.java
@@ -1,10 +1,20 @@
 package models;
 
+import java.util.Objects;
 
 public class Feedback {
 
     private String name;
     private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     private String message;
     private int id;
 
@@ -37,5 +47,22 @@ public class Feedback {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Feedback)) return false;
+        Feedback that = (Feedback) o;
+        return id == that.id &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(email, that.email) &&
+                Objects.equals(message, that.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, email, message, email, id);
     }
 }
