e1de0e1597b02e89e76f9476935537917de4136d
clarelimo <clare.limo@student.moringaschool.com>
2021-08-25 09:34:39 +0300

    kitchen model complete

--- glam-your-space/src/main/java/models/Kitchen.java
+++ glam-your-space/src/main/java/models/Kitchen.java
@@ -1,4 +1,27 @@
 package models;
 
-public class Kitchen {
+import java.util.Objects;
+
+public class Kitchen extends Design{
+    public Kitchen(String description, String place, String link){
+        this.description = description;
+        this.place = place;
+        this.link = link;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Kitchen)) return false;
+        Kitchen that = (Kitchen) o;
+        return id == that.id &&
+                Objects.equals(description, that.description) &&
+                Objects.equals(place, that.place) &&
+                Objects.equals(link,that.link);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(description,place, link, id);
+    }
 }
---
+++ glam-your-space/src/test/java/models/KitchenTest.java
@@ -0,0 +1,10 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class KitchenTest {
+    
+
+}
