4bc845cfc475307c294765ca44a525dc5ed5e223
clarelimo <clare.limo@student.moringaschool.com>
2021-08-25 10:01:03 +0300

    living room model complete

--- glam-your-space/src/test/java/models/LivingRoomTest.java
+++ glam-your-space/src/test/java/models/LivingRoomTest.java
@@ -5,5 +5,47 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class LivingRoomTest {
+    @Test
+    public void getDescriptionReturnsCorrectDescription() {
+        LivingRoom livingRoom = setupLivingRoom();
+        assertEquals("Get comfy", livingRoom.getDescription());
+    }
 
+    @Test
+    public void getPlaceReturnsCorrectPlace() {
+        LivingRoom livingRoom = setupLivingRoom();
+        assertEquals("mrp", livingRoom.getPlace());
+    }
+
+    @Test
+    public void getLinkReturnsCorrectLink() {
+        LivingRoom livingRoom = setupLivingRoom();
+        assertEquals("https://www.mrphome.com/", livingRoom.getLink());
+    }
+
+    @Test
+    public void setDescriptionSetsCorrectDescription() throws Exception {
+        LivingRoom livingRoom = setupLivingRoom();
+        livingRoom.setDescription("Relax");
+        assertNotEquals("The Aroma",livingRoom.getDescription());
+    }
+
+    @Test
+    public void setPlaceSetsCorrectPlace() throws Exception {
+        LivingRoom livingRoom = setupLivingRoom();
+        livingRoom.setPlace("odds and ends");
+        assertNotEquals("mrp",livingRoom.getPlace());
+    }
+
+    @Test
+    public void setLinkCorrectLink() throws Exception {
+        LivingRoom livingRoom = setupLivingRoom();
+        livingRoom.setLink("https://www.oddskenya.com/");
+        assertNotEquals("https://www.mrphome.com/",livingRoom.getLink());
+    }
+
+    //helper
+    public LivingRoom setupLivingRoom(){
+        return  new LivingRoom("Get comfy","mrp","https://www.mrphome.com/");
+    }
 }
